8760db16fd8628468f95159e1e07f06f9a355733
Adda1321 <adilmustafa13@gmail.com>
2024-03-07 09:43:37 +0500

    Folder directory setup

---
+++ src/DrawerComp.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+const PAGES = ["Home", "About Us", "Services", "Projects", "Contact Us"];
+
+const DrawerComp = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+    <React.Fragment>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <List>
+          {PAGES.map((page, index) => (
+            <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
+              <ListItemIcon>
+                <ListItemText>{page}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ color: "white", marginLeft: "auto" }}
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+export default DrawerComp;
