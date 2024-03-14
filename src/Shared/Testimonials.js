badc93b66fc6392eeedeb438adc1fb0ad0d454ef
Adda1321 <adilmustafa13@gmail.com>
2024-03-14 08:38:50 +0500

    Testimonial section

--- src/Shared/Testimonials.js
+++ src/Shared/Testimonials.js
@@ -1,36 +1,52 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css";
-import { EffectFade } from "swiper/modules";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import 'swiper/css/navigation';
+import '../Styles/Swiper.css';
+
+// import required modules
+import {Autoplay, FreeMode, Pagination , Mousewheel,Navigation } from 'swiper/modules';
+
 
 const Testimonials = ({ cardData }) => {
   return (
-    <div className="mt-10 p-10 mb-10">
-      <h4 className="text-center text-4xl">Testimonials</h4>
-      <Swiper slidesPerView={2} navigation modules={[Navigation]}>
-        {cardData.map((card) => (
-          <SwiperSlide>
-            <div className="card w-96 lg:w-96 bg-base-100 mt-20 min-h-96 mx-2 ml-40 shadow-xl rounded-xl text-center  bg-white">
-              <figure className="px-20 pt-10 mb-6 text-center">
-                {card.icon}
-              </figure>
-              <div className="card-body items-center ">
-                <h2 className="card-title text-lg md:text-xl lg:text-2xl mb-2">
-                  {card.title}
-                </h2>
-                <p className="text-content_color text-sm md:text-base lg:text-lg ">
-                  {card.description}
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
+    // <div className="mt-10 p-10 mb-10">
+    //   <h4 className="text-center text-4xl">Testimonials</h4>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        mousewheel={true}
+        // centeredSlides={true}
+        loop={true}
+        navigation={true}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: true,
+        }}
+                pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay,FreeMode, Mousewheel,Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className='swiper-slide-custom' >Slide 1</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 2</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 3</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 4</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 5</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 6</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 7</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 8</SwiperSlide>
+        <SwiperSlide className='swiper-slide-custom' >Slide 9</SwiperSlide>
       </Swiper>
-    </div>
+    // </div>
   );
 };
 
